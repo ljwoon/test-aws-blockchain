7af8d3f9e3528cd7de971f02edae481a649f22c2
ljwoon <ljwoon@nsmarts.co.kr>
2022-06-08 15:56:32 +0900

    update

--- vice-network/docker/caServerStart.sh
+++ vice-network/docker/caServerStart.sh
@@ -1,7 +1,7 @@
-export BYFN_CA1_PRIVATE_KEY=$(cd ../crypto-config/peerOrganizations/nsmarts.co.kr/ca && ls *_sk)
-export BYFN_CA2_PRIVATE_KEY=$(cd ../crypto-config/peerOrganizations/vice.com/ca && ls *_sk)
-export BYFN_CA3_PRIVATE_KEY=$(cd ../crypto-config/peerOrganizations/viceKR.com/ca && ls *_sk)
-export BYFN_CAO_PRIVATE_KEY=$(cd ../crypto-config/ordererOrganizations/example.com/ca && ls *_sk)
+# export BYFN_CA1_PRIVATE_KEY=$(cd ../crypto-config/peerOrganizations/nsmarts.co.kr/ca && ls *_sk)
+# export BYFN_CA2_PRIVATE_KEY=$(cd ../crypto-config/peerOrganizations/vice.com/ca && ls *_sk)
+# export BYFN_CA3_PRIVATE_KEY=$(cd ../crypto-config/peerOrganizations/viceKR.com/ca && ls *_sk)
+# export BYFN_CAO_PRIVATE_KEY=$(cd ../crypto-config/ordererOrganizations/example.com/ca && ls *_sk)
 
 # crypto-config 사용시 사용
 docker-compose -f ordererCA.yaml -f host1CA.yaml -f host2CA.yaml -f host3CA.yaml up -d
